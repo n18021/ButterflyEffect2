ef920f4dce6c3e85b5fdb78df4a6b1649a57ec2c
n18021 <39285118+n18021@users.noreply.github.com>
2019-10-22 02:07:13 +0900

    Add files via upload

---
+++ data/hotel-data.js
@@ -0,0 +1,52 @@
+export const HOTEL = [
+    new hotel(
+        "id",'1'
+        "name",'波の上クリスタルホテル',
+        "category",'ホテル',
+        "imageUrl",'https://pbs.twimg.com/profile_images/995418246995001344/gsNJkOhg.jpg',
+        "tel",'098-868-4190',
+        "address",'900-0031沖縄県那覇市若狭1-20-7',
+        "price",'8500'
+    )
+
+    new hotel(
+        "id",'2'
+        "name",'MrKINJOU'
+        "category",'ホテル',
+        "imageUrl",'http://mrkinjo.jp/wp/wp-content/uploads/2019/07/IMG_0651.jpg',
+        "tel",'098-851-8772',
+        "address",'沖縄県那覇市安次嶺1-8',
+        "price",'4860'
+    )
+    
+    new hotel(
+        "id",'3',
+        "name"'ダブルツリーbyヒルトン那覇',
+        "category",'ホテル',
+        "imageUrl"'https://doubletree.hiltonhotels.jp/static/upload/hotel_stay_20170619144052_lg_pc.jpg',
+        "tel",'098-862-0123',
+        "address",'900-0034 沖縄県那覇市東町3-15',
+        "price",''
+    )
+    
+    new hotel(
+        "id",'4',
+        "name",'OKINAWA KARIYUSHI LCH',
+        "category",'ビジネスホテル',
+        "imageUrl"'https://kariyushi-lch.jp/common/img/izumizaki_second/room/type_regular_01.jpg',
+        "tel",'098-860-1414',
+        "address",'900-0021 沖縄県那覇市泉崎1-14-6',
+        "price",'3500'
+    )
+
+    new hotel(
+        "id",'5',
+        "name"'沖縄ホテル',
+        "category",'観光ホテル',
+        "imageUrl",'https://www.okinawahotel.co.jp/wp-content/themes/okinawahotel/img/guestroom/r1.jpg',
+        "tel",'098-884-3191',
+        "address",'902-0066 沖縄県那覇市大道35番地',
+        "price",'4400'
+    )
+];
+
