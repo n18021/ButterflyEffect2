be2701ad491e6bcbe749455e838cdeb69462a572
n18021 <39285118+n18021@users.noreply.github.com>
2019-10-23 16:27:53 +0900

    Add files via upload

---
+++ data/amusement-data.js
@@ -0,0 +1,50 @@
+export  const Amusement = [
+    new Amusement(
+        "id"'1',
+        "name"'ラウンドワン宜野湾店',
+        "category"'sports',
+        "imageUrl"'https://www.round1.co.jp/service/spo-cha/img/about_spocha/about_spocha_mainv_left.jpg',
+        "tel"'098-870-2110',
+        "address",'901-2224 沖縄県宜野湾市真志喜3丁目28番8号',
+        "content"'ラウンドワンスタジアム沖縄・宜野湾店へようこそ！ ★全員で盛り上がる事間違いなし！『ボウリング』ムーンライトストライクゲーム！★人気景品は絶対にGET！『アミューズ』★島酒入荷してます♪',
+    )
+
+    new Amusement(
+        "id"'2',
+        "name"'サザンプレックス',
+        "category"'movie',
+        "imageUrl",'https://naokilife.com/wp-content/uploads/2019/04/2019423_190423_0028.jpg',
+        "tel"'098-936-7600',
+        "address",'901-1102 沖縄県島尻郡南風原町字宮城３７１−１',
+        "content"'私達は映画を通してハッピー空間の創造に取り組んでいきます。',
+    )
+
+    new Amusement(
+        "id"'3',
+        "name"'スカイレーン',
+        "category"'sports',
+        "imageUrl"'https://scontent-nrt1-1.xx.fbcdn.net/v/t1.0-9/42901873_1191708324337567_2143244850286297088_n.jpg?_nc_cat=106&_nc_oc=AQmHzQ2X9HuPX-MyZBMLmftMdNiv181r79S2UbznP44o8idjdp-CSOdiFY4ELgIkgYpRaVnAnJgFaNV2I5nA7PsM&_nc_ht=scontent-nrt1-1.xx&oh=48e972d5ebda41546f3ec4f786f02e8d&oe=5E5DB4F7',
+        "tel"'098-858-1005',
+        "address"'901-0146 沖縄県那覇市 具志875',
+    )
+
+    new Amusement(
+        "id"'4',
+        "name"'サザンヒル',
+        "category"'アミューズメント',
+        "imageUrl"'https://www.goyah.net/upfile/okinawa_date/sazan1.jpg',
+        "tel"'098-888-5858',
+        "address",'南風原町 字宮平 460-1',
+        "content"'28ｍ×58ｍのアイススケートリンク、36レーンのボウリング場、10打席のバッティングセンターなどのスポーツ施設や、岩盤浴で日頃のストレスを思い切り解消してみませんか？駐車場も300台収容出来るからゆったり！安心です。'
+    ) 
+    
+    new Amusement(
+            "id"'5',
+            "name"'カーニバルパーク美浜',
+            "category"'娯楽施設',
+            "imagUrl"'https://www.goyah.net/upfile/okinawa_date/parkmihama.jpg',
+            "tel"'098-987-5548',
+            "address"'北谷町 字美浜 15-69',
+            "content"'カーニバルパーク・ミハマは沖縄県中部の北谷町にあり、大きな観覧車は町のランドマークとなっています。施設内にはグルメ・ファッション・お土産・美容・ドラックストア・娯楽など、約50のテナントが入っています。'
+    
+    )
---
+++ data/leisure-data.js
@@ -0,0 +1,56 @@
+export const leisure[
+    new leisurei(
+        "id"'1',
+        "name"'やんばるンチャー',
+        "category"'自然のアクティビティ'
+        "imageUrl"'https://img.activityjapan.com/11/26527/11000002652707_MWT0pygI_4.JPG?version=1568515225',
+        "tel"' 0980-55-8118',
+        "address"'905-0022 沖縄県名護市世冨慶1084'
+        "price"'6500円'
+    "content"'バギー体験】女子にも大人気のバギーに乗って大自然を体感！4歳～お子様ご参加可★森散歩も楽しめます。自身でバギーを運転し、オフロードや森林コースを楽しめます。また途中には素敵な沖縄北部「通称やんばる」の森林散策も楽しめます。'
+    )
+
+    new leisurei(
+        "id"'2',
+        "name"'ちゅら海水族館',
+        "category"'水族館'
+        "imageUrl"'https://churaumi.okinawa/userfiles/images/haisai/image-01.jpg',
+        "tel"'0980-48-3748',
+        "address"'905-0206 沖縄県国頭郡本部町字石川424番地'
+        "price"'1880円',
+        "content"'水族館では、神秘に満ちた沖縄の生き物たちの雄大な世界が広がります。 太陽の光が降りそそぐ「サンゴの海」水槽では大規模なサンゴの飼育展示をご覧になれます。そして、世界最大の魚ジンベエザメや、世界初の繁殖に成功したナンヨウマンタが観察できる大迫力の巨大水槽「黒潮の海」。さらに謎に包まれた沖縄の深海を再現した「深層の海」水槽へと、沖縄の海を丸ごと体感できます。 何度訪れても新しい出会いと発見がある「沖縄美ら海水族館」へ、みなさまのご来館をお待ちしております。'
+    )
+
+    new leisurei(
+        "id"'3'
+        "name"'沖縄ワールド'
+        "category"'観光テーマパーク',
+        "imageUrl"'https://www.gyokusendo.co.jp/okinawaworld/about/images/cave.jpg'
+        "tel"'098-949-7421',
+        "address"'沖縄県南城市玉城前川１３３６'
+        "price"'1700円',
+        "content"'おきなわワールドへは那覇空港から車で約30分！沖縄の自然と文化、歴史が体感できる県内最大級の観光テーマパークです。'
+    )
+
+    new leisurei(
+        "id"'4',
+        "name"'ビオスの丘',
+        "category"'自然体験施設',
+        "imageUrl"'https://www.bios-hill.co.jp/event/wp-content/uploads/sites/2/2015/09/4708e345300131b13f0ba360328807da-e1451700391775.png',
+        "tel"'098-965-3400',
+        "address"'沖縄県うるま市石川嘉手苅 961番の30',
+        "price"'1700円',
+        "content"'自然が今なお隣り合わせにある沖縄だからこそ、学びがいっぱい。生き物との触れあいや自然・文化・歴史から生まれた遊びは、子どもから大人まで、「こころ育む自然体験」を体感いただけます。'
+    )
+
+    new leisurei(
+        "id"'5',
+        "name"'BLUE SEAL ICE PARK',
+        "category"'テーマパーク',
+        "imageUrl"'https://d2goguvysdoarq.cloudfront.net/system/facility_photos/photos/124573/normal.jpg?1522741226',
+        "tel"'098-988-4535',
+        "address"'沖縄県浦添市牧港5-5-6',
+        "price"'1500円',
+        "content"'「沖縄の次世代を担う子どもたちに、アイスクリームを通じて夢を与えられる施設を作りたい」そんな願いを温め続け、ついにブルーシールアイスパークが実現しました。約70年の歴史を伝えるヒストリー展示をはじめ、アイスクリーム製造現場の見学そして県内初のアイスバーデコレーション体験ができます。'
+    )
+]
