74696c446aa19418a280793cd1dc0f80c4d86b1c
n18021 <39285118+n18021@users.noreply.github.com>
2019-10-24 11:48:41 +0900

    Add files via upload

---
+++ data/present.js
@@ -0,0 +1,55 @@
+export const  PRESENT = [
+    new Present(
+        "id",'1',
+        "name",'ミヤギヤブルースポット',
+        "category",'gift',
+        "imageUrl",'https://www.google.com/search?q=%E3%83%9F%E3%83%A4%E3%82%AE%E3%83%A4%E3%83%96%E3%83%AB%E3%83%BC%E3%82%B9%E3%83%9D%E3%83%83%E3%83%88+%E9%82%A3%E8%A6%87&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiV7fG70rHlAhULZt4KHSe3CTIQ_AUIEygC&biw=1299&bih=637#imgrc=1kQ0jrpMcDIptM:',
+        "tel"'098-869-1426',
+        "address",'沖縄県那覇市松尾2‐12‐22',
+        "content",'カフェやバー、雑貨屋さん、古着屋さん、美容院…おしゃれなお店が立ち並ぶ那覇市の浮島通りから開南中央通りを入った角に、つい長居したくなってしまうセレクトショップがあります。'
+    )
+
+
+    new Present(
+        "id",'2',
+        "name",'花の藤商',
+        "category",'gift',
+        "imageUrl",'http://www.hananofujisho.com/img/shop_img2.png',
+        "tel"'098-862-7793',
+        "address",'900-0015 那覇市久茂地2-22-12 1F',
+        "content"'店内には沢山のお花を取り揃えております。お気軽に御利用下さいませ。スタッフ一同心よりお待ち申し上げております。',
+    )
+
+
+    new Present(
+        "id",'3',
+        "name",'山城時計店那覇メインプレイス店',
+        "category",'gift',
+        "imageUrl"'https://r-contents.weddingpark.net/resize/720x540/brand_selectshops/300092/main/98aabb9d04b37a2b36435af146d8a0fb6d0e19600512167001562294847.800x600.jpg',
+        "tel",'098-951-3322',
+        "address",'沖縄県那覇市おもろまち4−4−9那覇メインプレイス1F'       
+        "content",'山城時計店では、ブライダルジュエリーブランド「俄NIWAKA」「CHRISTIAN BAUER」「カフェリング」「Makana」などの注目のブランド30種類以上のリングを用意。',
+    )
+
+
+    new Present(
+        "id",'4',
+        "name",'４℃ ブライダル',
+        "category"'gift',
+        "imageUrl",'https://4-bridal.jp/cms/images/shop/38030_1.jpg'
+        "tel",'098-868-6751',
+        "address",'沖縄県那覇市久米2-4-13 明治安田生命沖縄ビル1F',
+        "content",'那覇市の人気観光地「国際通り」からほど近い当店は、沖縄で数少ないブライダルリング専門店です。高品質のダイヤモンドや、お二人の誓いの証にふさわしい上質な結婚指輪を豊富にご用意しております。'
+    )
+
+
+    new Present(
+        "id",'5',
+        "name",'JoMalone',
+        "category",'gift',
+        "imageUrl",'https://www.dfs.com/medias/BMP-Brand-Image-Desktop-492-X-620px.jpg?context=bWFzdGVyfGltYWdlc3wxMDk3MDZ8aW1hZ2UvanBlZ3xpbWFnZXMvaGMwL2hkMy8xMDI0MTM5Njg2NzEwMi5qcGd8MTExYzBjMzg3MTExNTQxODc5OWY2MDI0MTZjZDBhM2M1ZjVlNjUwMWMyYjhmYTgwZjg0NTQzNjVkMTk1ZjcxZQ&impolicy=resize&pquality=high&width=492',
+        "tel",'0120-782-460',
+        "address",'沖縄県那覇市おもろまち４－１',
+        "content",'沖縄県那覇市でジョー マローン ロンドン（Jo Malone London）を扱う直営店情報。沖縄県那覇市のジョー マローン ロンドン（Jo Malone London）の商品・アイテムを販売する店舗をチェックしよう！',
+    )
+]
---
+++ data/restrant-data.js
@@ -0,0 +1,61 @@
+export const RESTRANT = [
+    new restrant(
+        "id",'1',
+        "name",'ハンタバール',
+        "category",'restrant',
+        "imageUrl",'https://cdn.amebaowndme.com/madrid-prd/madrid-web/images/sites/57415/bc2c61a8eae65a7ba387969dfb55ae8b_b1a406b2e368389e4523d23bfe1a2fb0.jpg?width=800',
+        "tel",'098-895-3338',
+        "address",'901-2416 沖縄県中頭郡中城村字和宇慶４６８',
+        "budget",'￥4800~￥5000,'
+        "content",'イタリアンテイスト 中城湾を眺望する レストラン＆ダイニングbar 友人 大切な人東海岸を望む緑に囲まれたRISTRANTE & BAR。 昼から夜にかけてガラリと変わる風景と共に、イタリアンテイストの料理・ワインをご堪能下さい。'
+    )
+]
+
+    new restrant(
+        "id",'2',
+        "name",'隠れ家レストランKOB',
+        "category",'restrant',
+        "imageUrl",'http://finedays.ginowan.or.jp/wp-content/uploads/2016/12/bc7908a076cb66578a2e8e9ccfe9ecc2-1024x683.jpg',
+        "tel",'098-943-4341',
+        "address",'沖縄県宜野湾市大山1-12-32',
+        "budget",'￥3000~￥3999',
+        "content",'宜野湾市大山の高台に位置するKOBAでは、青い海と空、そして宜野湾市の街並を眺めながら沖縄県産食材を使ったフレンチベースのランチが楽しめる隠れ家レストラン。フレンチの要と言っても過言ではないソースや、ランチに付くパンやデザートもすべて手作りというこだわり。'
+    )
+
+    
+    new restrant(
+        "id",'3',
+        "name",'デッカーズキッチン',
+        "category",'restrant',
+        "imageUrl",'https://www.okinawa-americanvillage.com/amevi/wp-content/uploads/2013/09/deckers01.jpg',
+        "tel",'098-989-5307',
+        "address",'904-0115 沖縄県中頭郡北谷町字美浜９−１',
+        "budget",'￥2,000～￥2,999'
+        "content",'北谷町美浜のデポアイランドにあるナチュラルダイニング「デッカーズキッチン」は、気持ちの良い朝を迎えたときのような爽やかな雰囲気の明るい店内で、ゆったりとしたリゾート感たっぷりのカフェレストラン。'
+    )
+
+
+    new restrant(
+        "id",'4',
+        "name",'HONOKA',
+        "category",'restrant',
+        "imageUrl",'https://proxy.weban.jp/Image/9167407.jpg',
+        "tel",'098-863-6960',
+        "address",'900-0032 沖縄県那覇市松山１丁目６−１４ エンブレム松山 1F',
+        "budget",'￥5,000 ~ ￥7,000',
+        "content",'朝10時まで営業。飲み放題もお手頃、時間を忘れて過ごせる居酒屋',
+    )
+
+
+    new restrant(
+        "id",'5',
+        "name",'通堂',
+        "category",'restrant',
+        "imageUrl",'http://okinawa-realguide.com/wp-content/uploads/2017/06/tondou_appearance-1024x683.jpg',
+        "tel",' 098-857-5577',
+        "address",'901-0155 沖縄県那覇市 金城５－４－６',
+        "budget",'￥1,000 ~ ￥2,000',
+        "content",'和風で落ち着いた雰囲気のカウンター席もある店内で、豚骨スープや塩味ベースの様々な種類のラーメンが頂ける。',
+    )
+
+]
